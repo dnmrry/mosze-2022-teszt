499b771a129f2b716bd21e0a53152cb1dc2731c2
Muszáj Gábor Dániel <72542379+dnmrry@users.noreply.github.com>
2022-09-10 19:48:58 +0200

    fix bugs

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
+#include <iostream>
+
+constexpr int N_ELEMENTS = 100;
+
 int main()
 {
-    // elírt változónév
+    int *b = new int[N_ELEMENTS];
 
-    int *b = new int[NELEMENTS];
-    // aposztróf idézőjel helyett
-
-    std::cout << '1-100 ertekek duplazasa'
-   // nincs feltétel
-
-    for (int i = 0;)                           
-    {
-      // hibás hivatkozás (kimaradt a * és a +1)
-
-        b[i] = i * 2;                                             
+    std::cout << "1-100 ertekek duplazasa\n";    
+    
+    for (int i = 0; i < N_ELEMENTS; i++){
+        *(b + i) = (i + 1) * 2;
     }
-     // hiányos feltétel for ciklusban (N_ELEMENTS)
 
-    for (int i = 0; i; i++)                                       
-    {
-         // lemaradt hivatkozás ( << *(b + i) << '\n';)
+    for (int i = 0; i < N_ELEMENTS; i++){
+        std::cout << "Ertek: " << *(b + i) << '\n';
+    }        
 
-        std::cout << "Ertek:"                                     
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;    
-    // inicializálatlan változó (atlag = 0)
+    
+    std::cout << "Atlag szamitasa: " << std::endl;
 
-    int atlag;      
-     // pontosvessző helyett vessző                                             
-    for (int i = 0; i < N_ELEMENTS, i++)                          
-    {
-        // lemaradt a * és a ;
-        atlag += b[i]                                             
+
+    int atlag = 0;
+   
+    for (int i = 0; i < N_ELEMENTS; i++){
+        
+        atlag += *(b + i);
     }
+
     atlag /= N_ELEMENTS;
+
+
     std::cout << "Atlag: " << atlag << std::endl;
+
+
+    return 0;
+}
