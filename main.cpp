bdb35bc011b33d8b6994468c2ba89ea5a8b577a3
Muszáj Gábor Dániel <72542379+dnmrry@users.noreply.github.com>
2022-09-10 19:42:38 +0200

    commenting errors

--- main.cpp
+++ main.cpp
@@ -1,26 +1,36 @@
-#include <iostream>
-
-constexpr int N_ELEMENTS = 100;
-
 int main()
 {
+    // elírt változónév
+
     int *b = new int[NELEMENTS];
+    // aposztróf idézőjel helyett
+
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+   // nincs feltétel
+
+    for (int i = 0;)                           
     {
-        b[i] = i * 2;
+      // hibás hivatkozás (kimaradt a * és a +1)
+
+        b[i] = i * 2;                                             
     }
-    for (int i = 0; i; i++)
+     // hiányos feltétel for ciklusban (N_ELEMENTS)
+
+    for (int i = 0; i; i++)                                       
     {
-        std::cout << "Ertek:"
+         // lemaradt hivatkozás ( << *(b + i) << '\n';)
+
+        std::cout << "Ertek:"                                     
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    std::cout << "Atlag szamitasa: " << std::endl;    
+    // inicializálatlan változó (atlag = 0)
+
+    int atlag;      
+     // pontosvessző helyett vessző                                             
+    for (int i = 0; i < N_ELEMENTS, i++)                          
     {
-        atlag += b[i]
+        // lemaradt a * és a ;
+        atlag += b[i]                                             
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
-}
